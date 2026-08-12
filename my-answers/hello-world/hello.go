466dcc7a43be0929957c6aacff2db98bfd7cf404
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-08-12 14:05:38 +0200

    feat(hello-world): support multi-language greetings
    Add name-default, Spanish, and French handling via a switch,
    extracted into a greetingPrefix helper. Extends the initial
    Hello(name) implementation from the previous commit.

--- my-answers/hello-world/hello.go
+++ my-answers/hello-world/hello.go
@@ -2,10 +2,38 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	english            = "English"
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	s := greetingPrefix(language)
+	return s + name
+}
+
+func greetingPrefix(language string) string {
+	if language == "" {
+		language = english
+	}
+
+	switch language {
+	case spanish:
+		return spanishHelloPrefix
+	case french:
+		return frenchHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
